aa1a1c9c9cca2ff599bbca70425ebe05a4587914
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-10-03 18:51:22 -0300

    Output prints result of genome joins

--- proj_pro.pl
+++ proj_pro.pl
@@ -1,21 +1,14 @@
 % ["xxxxxababababyyyyyy","yyaaaaaaaaaaa","yyyyyyeeeeeeeeeeeeee","cccccccccccccccxxxxx","fffffffffffffffwwwwww","wwwwwwgggggggggggxx"]
-output([L|Ls]) :- combine(L,Ls,Z), show(Z).
-show([Z|Zs]) :- format("~s~n",Z), nl, show(Zs).
+output(L) :- iterate(L,R), show(R).
+show([Z|Zs]) :- format("~s~n",[Z]), nl, show(Zs).
 
+iterate([],[]).
 iterate([L|Ls],R) :-
-  combine(L,Ls,H,Hs) -> iterate([H|Hs],R) ; iterate(Hs,Z), R = [H|Z].
+  combine(L,Ls,H,Hs) -> iterate([H|Hs],R) ; (iterate(Ls,Z), R = [L|Z]).
 
 % Try to combine strings A-B or B-A util match or list end.
-combine(_,[],[]).
 combine(H,[L|Ls],R,Rs) :- (joinstr(H,L,R) ; joinstr(L,H,R)), Rs = Ls.
 combine(H,[L|Ls],R,Rs) :- combine(H,Ls,R,T), Rs = [L|T].
-
-% Check if B can be appended to A, returns the instersection.
-intersect([A|As],B,C) :-
-    substr([A|As],B) -> C = [A|As] ; intersect(As,B,C).
-
-substr([],_).
-substr([A|As],[B|Bs]) :- A==B, substr(As,Bs).
 
 % Join strings
 joinstr(A,B,L) :-
@@ -23,3 +16,9 @@ joinstr(A,B,L) :-
 
 drop(B,0,B).
 drop([_|Bs],N,R) :- X is N-1, drop(Bs,X,R).
+
+% Check if B can be appended to A, returns the instersection.
+intersect([A|As],B,C) :- substr([A|As],B) -> C = [A|As] ; intersect(As,B,C).
+
+substr([],_).
+substr([A|As],[B|Bs]) :- A==B, substr(As,Bs).

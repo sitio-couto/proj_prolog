405eac8f4a8c3257d688a6d2bafbf7152a14bd4d
Vinicius Couto Espindola <vinicius.c.e@hotmail.com>
2018-10-03 13:19:48 -0300

    Changed joinstr: only joins if > 3, and returns the original string otherwise.

--- proj_pro.pl
+++ proj_pro.pl
@@ -1,5 +1,4 @@
-
-
+% ["xxxxxababababyyyyyy","yyaaaaaaaaaaa","yyyyyyeeeeeeeeeeeeee","cccccccccccccccxxxxx","fffffffffffffffwwwwww","wwwwwwgggggggggggxx"]
 
 % Check if B can be appended to A, returns the instersection.
 intersect([A|As],B,C) :-
@@ -9,7 +8,9 @@ substr([],_) :- !.
 substr([A|As],[B|Bs]) :- A==B, substr(As,Bs).
 
 % Join strings
-joinstr(A,B,I,L) :- length(I,N), drop(B,N,R), append(A,R,L).
+joinstr(A,B,L) :-
+  intersect(A,B,I), length(I,N),
+  N > 3 -> drop(B,N,R), append(A,R,L) ; L = A.
 
 drop(B,0,B).
 drop([_|Bs],N,R) :- X is N-1, drop(Bs,X,R).

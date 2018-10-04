743ad8bcce6e4ad0087c73fb4e8a9999d7b01773
Luciano Gigantelli Zago <ra182835@students.ic.unicamp.br>
2018-10-03 23:23:29 -0300

    Read input from stdin, write output to stdout

--- proj_pro.pl
+++ proj_pro.pl
@@ -1,15 +1,20 @@
-% ["xxxxxababababyyyyyy","yyaaaaaaaaaaa","yyyyyyeeeeeeeeeeeeee","cccccccccccccccxxxxx","fffffffffffffffwwwwww","wwwwwwgggggggggggxx"]
-
 :- dynamic seq/1.
 
-seq("xxxxxababababyyyyyy").
-seq("yyaaaaaaaaaaa").
-seq("yyyyyyeeeeeeeeeeeeee").
-seq("cccccccccccccccxxxxx").
-seq("fffffffffffffffwwwwww").
-seq("wwwwwwgggggggggggxx").
+% Main function, read input, compute, print output
+main :-
+    readpred,
+    findall(_,pred(seq(_),seq(_),_),_),
+    findall(A, seq(A), E), maplist(writeln, E).
 
-% findall(S,(pred(seq(A),seq(B),X),string_codes(S,X)),L).
+% Read input and assert predicate
+readpred :-
+    repeat,
+    read_string(user_input, "\n", "\r\t ", End, String),
+    ( End == -1
+      -> !;
+      assertz(seq(String)),
+      fail
+    ).
 
 % Check if B can be appended to A, returns the instersection.
 intersect([A|As],B,C) :-
@@ -19,17 +24,17 @@ substr([],_) :- !.
 substr([A|As],[B|Bs]) :- A==B, substr(As,Bs).
 
 % Manage predicates
-pred(seq(A),seq(B),Cstring) :-
-  seq(A), seq(B), A\=B,
-  string_codes(A,Alist), string_codes(B,Blist),
-  joinstr(Alist,Blist,Clist), string_codes(C,Clist),
-  retract(seq(A)), retract(seq(B)),
-  not(seq(C)), assertz(seq(C)).
+pred(seq(A),seq(B),C) :-
+    seq(A), seq(B), A\=B,
+    string_codes(A,Alist), string_codes(B,Blist),
+    joinstr(Alist,Blist,Clist), string_codes(C,Clist),
+    retract(seq(A)), retract(seq(B)),
+    not(seq(C)), assertz(seq(C)).
 
 % Join strings
 joinstr(A,B,L) :-
-  intersect(A,B,I), length(I,N),
-  N > 3 -> drop(B,N,R), append(A,R,L).
+    intersect(A,B,I), length(I,N),
+    N > 3 -> drop(B,N,R), append(A,R,L).
 
 drop(B,0,B).
 drop([_|Bs],N,R) :- X is N-1, drop(Bs,X,R).
